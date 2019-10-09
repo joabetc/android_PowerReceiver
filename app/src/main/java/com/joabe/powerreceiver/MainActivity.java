3e39d6541d03776b7bef8c3880fc57b946ad14ea
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-09 16:42:48 -0300

    Creating an intent filter with Intent actions

--- app/src/main/java/com/joabe/powerreceiver/MainActivity.java
+++ app/src/main/java/com/joabe/powerreceiver/MainActivity.java
@@ -2,6 +2,8 @@ package com.joabe.powerreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
     }
 }
